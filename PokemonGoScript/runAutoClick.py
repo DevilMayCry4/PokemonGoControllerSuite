ec1d934f2d23cdfb4acd10c4ab9871bb35de9e40
virgil <237326369@qq.com>
2016-07-14 00:06:45 +0800

    add
    add

--- PokemonGoScript/runAutoClick.py
+++ PokemonGoScript/runAutoClick.py
@@ -1,14 +1,24 @@
 import os
 import time
+import sys,md5
+
+oldfile = ''
 
 def clickAction():
-	os.system("./autoClicker -x 539 -y 508")
-	os.system("./autoClicker -x 539 -y 558")
+	os.system("./autoClicker -x 1433 -y 536")
+	os.system("./autoClicker -x 1433 -y 588")
 	time.sleep(1)
 	print "clicking!!"
 
 def start():
+	global oldfile
 	while True:
-		clickAction()
+		f = open('/Users/virgilwu/Downloads/PokemonGoControllerSuite-master/PokemonHandler/PokemonHandler/pokemonLocation.gpx','r')
+		md5New = md5.new(f.read()).digest()
+		if oldfile == '' or md5New != oldfile:
+			oldfile = md5New
+			clickAction()
+		else:
+			time.sleep(1)
 
 start()
